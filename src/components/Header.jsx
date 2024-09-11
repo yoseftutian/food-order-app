f76cfe316cbf40ad4510a284fc93a3bf763a3adb
yosef-tutian <yoseftutian@gmail.com>
2024-09-11 17:26:43 +0300

    adding MealItem component to every meal item

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <header id="main-header">
       <div id="title">
-        <img src="{logoimg}" alt="A restaurant" />
+        <img src={logoimg} alt="A restaurant" />
         <h1>ReactFood</h1>
       </div>
       <button>Cart(0)</button>
