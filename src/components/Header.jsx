995f847567478eb61341e3824b65f8f7d70015c9
yosef-tutian <yoseftutian@gmail.com>
2024-09-11 16:47:47 +0300

    Adding components and connecting to the server

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logoimg from "../assets/logo.jpg";
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src="{logoimg}" alt="A restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+      <button>Cart(0)</button>
+      <nav />
+    </header>
+  );
+}
