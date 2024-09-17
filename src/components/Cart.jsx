38603e23b9dcb71a9c90e7dec0027a77b9825762
yosef-tutian <yoseftutian@gmail.com>
2024-09-17 20:55:35 +0300

    add UserProgressContextProvider component

---
+++ src/components/Cart.jsx
@@ -0,0 +1,39 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formatting";
+import Button from "./UI/button";
+import UserProgressContext from "../store/UserProgressContext";
+
+export default function Cart() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
+    0
+  );
+
+  function handleCloseCrt() {
+    userProgressCtx.hideCart();
+  }
+  return (
+    <Modal className="cart" open={userProgressCtx.progress === "cart"}>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartCtx.items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+      <p className="modal-actions">
+        <Button onClick={handleCloseCrt} textOnly>
+          Close
+        </Button>
+        <Button onClick={handleCloseCrt}>Go to Checkout</Button>
+      </p>
+    </Modal>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,17 @@ import { useContext } from "react";
 import logoimg from "../assets/logo.jpg";
 import Button from "./UI/button";
 import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
 export default function Header() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
   return (
     <header id="main-header">
       <div id="title">
@@ -14,7 +20,9 @@ export default function Header() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>
+          Cart({totalCartItems})
+        </Button>
       </nav>
     </header>
   );
